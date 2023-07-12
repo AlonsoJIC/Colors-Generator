8762b10b1de37a373bbd8009641358dd7c5923bb
AlonsoJIC <jalonsojic@gmail.com>
2023-07-12 03:15:27 -0600

    v2, agregar color,  quitar color, titulo

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
-let colorOne = document.getElementById("color-a");
-let colorTwo = document.getElementById("color-b");
+let colorOne = document.getElementById("color-1");
+let colorTwo = document.getElementById("color-2");
 let currentDirection = 'to bottom';
 let outputCode = document.getElementById("code");
+let addColorBtn = document.getElementById("addColorBtn");
+let colorInputs = document.getElementById("colorInputs");
+let colors = [colorOne.value, colorTwo.value];
+
+addColorBtn.addEventListener("click", addColorInput);
+colorOne.addEventListener("input", updateGradient);
+colorTwo.addEventListener("input", updateGradient);
 
 function setDirection(value, _this) {
-    let direcrtions = document.querySelectorAll(".buttons button");
-    for (let i of direcrtions) {
-        i.classList.remove("active");
-    }
-    _this.classList.add("active");
-    currentDirection = value;
+  let directions = document.querySelectorAll(".buttons button");
+  for (let i of directions) {
+    i.classList.remove("active");
+  }
+  _this.classList.add("active");
+  currentDirection = value;
+  updateGradient();
 }
 
-function generateCode() {
-    outputCode.value = `background-image: linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
-    document.getElementsByTagName("BODY")[0].style.backgroundImage = `linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
+function updateGradient() {
+  let gradientColors = Array.from(colorInputs.getElementsByTagName("input")).map(input => input.value);
+  outputCode.value = `background-image: linear-gradient(${currentDirection}, ${gradientColors.join(", ")})`;
+  document.getElementsByTagName("BODY")[0].style.backgroundImage = `linear-gradient(${currentDirection}, ${gradientColors.join(", ")})`;
 }
 
-function copyText() {
-    outputCode.select();
-    document.execCommand('copy');
-    alert("Gradient Copied!");
+function addColorInput() {
+  let newColorInput = document.createElement("input");
+  newColorInput.type = "color";
+  newColorInput.addEventListener("input", updateGradient);
+
+  let removeColorBtn = document.createElement("button");
+  removeColorBtn.innerHTML = "Delete";
+  removeColorBtn.addEventListener("click", function() {
+    removeColorInput(newColorInput);
+  });
+
+  let colorInputContainer = document.createElement("div");
+  colorInputContainer.appendChild(newColorInput);
+  colorInputContainer.appendChild(removeColorBtn);
+
+  colorInputs.appendChild(colorInputContainer);
+  colors.push(newColorInput.value);
+  updateGradient();
 }
 
+function removeColorInput(inputElement) {
+  if (colorInputs.childElementCount <= 2) {
+    alert("Debes mantener al menos 2 colores");
+    return;
+  }
 
-generateCode();
+  let colorInputContainer = inputElement.parentNode;
+  let index = Array.from(colorInputContainer.parentNode.children).indexOf(colorInputContainer);
+  colors.splice(index, 1);
+  colorInputs.removeChild(colorInputContainer);
+  updateGradient();
+}
+
+updateGradient();
